af31179facc77aa2e66783f84897d16655a98fe8
jessetvogel <jesse.t.vogel@gmail.com>
2021-02-03 14:15:22 +0100

    ✨ Some more definitions

--- python/data.py
+++ python/data.py
@@ -1,6 +1,7 @@
 data = {
     "CT": [ "category-theory.tex" ],
     "CA": [ "commutative-algebra.tex",
+            "modules.tex",
             "fields.tex" ],
     "AT": [ "algebraic-topology.tex" ],
     "DG": [ "differential-geometry.tex" ],
