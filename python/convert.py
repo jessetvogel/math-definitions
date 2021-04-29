fd2984f4b24869f72676d154dfcd45f63a42587a
jessetvogel <jesse.t.vogel@gmail.com>
2021-04-29 11:26:28 +0200

    🧱 Restructured files, and added some DG, AG, TO

--- python/convert.py
+++ python/convert.py
@@ -1,26 +1,35 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[2]:
+# In[1]:
 
 
-from data import data
+import os
 from tex2html import Parser
 
 
-# In[3]:
+# In[2]:
 
 
 # Create html files
 parser = Parser('../data/')
 
-for c in data:
-    parser.set_prefix(c)
-    for f in data[c]:
-        parser.parse('../tex/' + f)
+# Search .tex files inside the two-lettered category-directories
+tex_path = '../tex/'
+categories = sorted([ f for f in os.listdir(tex_path) if os.path.isdir(os.path.join(tex_path, f)) ])
+for cat in categories:
+    cat_path = os.path.join(tex_path, cat)
+    parser.set_prefix(cat)
+    for f in os.listdir(cat_path):
+        file_path = os.path.join(cat_path, f)
+        file_extension = os.path.splitext(file_path)[1]
+        if '--' in file_path or file_extension != '.tex' or not os.path.isfile(file_path):
+            continue
+            
+        parser.parse(file_path)
 
 
-# In[4]:
+# In[3]:
 
 
 # Write topics.js
@@ -34,7 +43,7 @@ topics_js.write('}\n')
 topics_js.close()
 
 
-# In[8]:
+# In[4]:
 
 
 # Write examples.js
@@ -48,7 +57,7 @@ examples_js.write('];\n')
 examples_js.close()
 
 
-# In[4]:
+# In[5]:
 
 
 # Print stats
