7aea21e61397ede33ed7d7508eaa610ee77ff3b8
jessetvogel <jesse.t.vogel@gmail.com>
2022-02-18 11:49:24 +0100

    🙃 Batch of definitions

--- js/categories.js
+++ js/categories.js
@@ -7,6 +7,7 @@ const categories = {
     "DG": "Differential Geometry",
     "DT": "Deformation Theory",
     "FA": "Functional Analysis",
+    "GM": "General",
     "GT": "Group Theory",
     "HA": "Homological Algebra",
     "HT": "Homotopy Theory",
